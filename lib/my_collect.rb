5212c21f8121380726b31a8c379761d11612f5e0
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 07:55:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
-require "pry"
+Require "pry"
+
 def my_collect(array)
   new_array = []
   i = 0
   while i < array.length
     new_array << yield(array[i])
     i += 1
-    # binding.pry
+    #binding.pry
   end
-return new_array  ```return it here
+return new_array
 end
